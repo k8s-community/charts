8a55d08e7453529eeab203ed344f6499fc2f6d26
Elena Grahovac <elena@grahovac.me>
2017-05-19 00:24:20 +0700

    Update charts version - add github-integraion

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,6 @@
 helm package charts
 helm package oauth-proxy
 helm package user-manager
+helm package github-integration
 mv *.tgz packages/
 helm repo index packages --url https://services.k8s.community/charts
